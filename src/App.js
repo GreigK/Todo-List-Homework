e013aabd364eaa612810f1f6e64c1abce3b6942c
Greig Kerr <greigkerr@virginmedia.com>
2022-08-15 18:58:15 +0100

    MVP and Some Extensions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [todos, setTodo] = useState([
+    {task: "Take out the bins", priority: "high"},
+    {task: "MOT", priority: "low"},
+    {task: "Clean stuff", priority: "high"},
+  ])
+  const [newTodo, setNewTodo] = useState("");
+  const [newPrio, setNewPrio] = useState("");
+
+  const todoNodes = todos.map((todo, index) => {
+    return <li key={index}>
+        <span>{todo.task}, {todo.priority}</span>
+      {/* {todo.isPriority ? <span classname="priority"> high</span> : <button onClick={() => {priorityTask(index)}}>Low</button> } */}
+    </li>
+  })
+
+  const handleTaskInput = (task) => {
+    setNewTodo(task.target.value)
+  }
+
+  const handlePrioInput = (priority) => {
+    setNewPrio(priority.target.value)
+  }
+
+  const saveNewTodo = (task) => {
+    task.preventDefault();
+    const copyTask = [...todos]
+    copyTask.push({task: newTodo, priority: newPrio})
+    setTodo(copyTask);
+    setNewTodo("")
+  }
+
+  // const priorityTask = (index) => {
+  //   const copyTask = [...todos];
+  //   copyTask[index].priority = "low";
+  //   setTodo(copyTask)
+  // }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <h1>Todo List</h1>
+    <hr></hr>
+
+    <ul>
+      {todoNodes}
+    </ul>
+
+    <form onSubmit={saveNewTodo}>
+      <label htmlFor="new-task">Add new task:</label>
+      <input id="new-task" type="text" value={newTodo} onChange={handleTaskInput}/>
+      <input type="radio" name="prio" value="High" onChange={handlePrioInput}/>High
+      <input type="radio" name="prio" value="Low" onChange={handlePrioInput}/>Low
+      <input type="submit" value="Save New Task"/>
+    </form>
+    
+
+  </div>
   );
 }
 
